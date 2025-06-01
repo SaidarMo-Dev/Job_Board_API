350cccad40c5b8214db4e8f9c3f7c0e0f2ace21b
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-01 19:19:57 +0200

    Add Country Controller

---
+++ JobBoard.Api/Controllers/CountryController.cs
@@ -0,0 +1,30 @@
+﻿using JobBoard.Api.Bases;
+using JobBoard.Core.Feutures.Countries.Queries.Models;
+using JobBoard.Data.Metadata;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace JobBoard.Api.Controllers
+{
+	[ApiController]
+	[AllowAnonymous]
+	public class CountryController : AppControllerbase
+	{
+		[AllowAnonymous]
+		[SwaggerOperation(
+			summary: "Retrieves Country Details by Id",
+			OperationId = "GetCountryById",
+			Description = "This EndPoint fetch Country Details. Country must exist"
+
+		)]
+
+		[HttpGet(Router.CountryRoute.GetByID)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public async Task<IActionResult> FindCountryById([FromRoute] int Id)
+		{
+			return NewResult(await Mediator.Send(new GetCountryByIdQuery(Id)));
+		}
+	}
+}
