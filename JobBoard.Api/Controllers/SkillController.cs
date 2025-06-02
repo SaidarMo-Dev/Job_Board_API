e5add83201432afc11b810b29aeb12bae0918d2d
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-02 19:47:34 +0200

    Add Swagger Description for skill

--- JobBoard.Api/Controllers/SkillController.cs
+++ JobBoard.Api/Controllers/SkillController.cs
@@ -4,6 +4,7 @@ using JobBoard.Core.Feutures.Skills.Queries.Models;
 using JobBoard.Data.Metadata;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace JobBoard.Api.Controllers
 {
@@ -12,6 +13,10 @@ namespace JobBoard.Api.Controllers
 	public class SkillController : AppControllerbase
 	{
 
+		[SwaggerOperation(
+			Summary = "Get skill by ID",
+			Description = "Retrieves detailed information about a specific skill identified by its ID.",
+			OperationId = "GetSkillById")]
 
 		[HttpGet(Router.SkillRoute.GetByID)]
 		public async Task<IActionResult> GetSkillById([FromRoute] int Id)
@@ -20,12 +25,23 @@ namespace JobBoard.Api.Controllers
 
 		}
 
+		[SwaggerOperation(
+			Summary = "Get all skills",
+			Description = "Returns a list of all skills available in the system.",
+			OperationId = "GetAllSkills")]
+
 		[HttpGet(Router.SkillRoute.GetAll)]
 		public async Task<IActionResult> GetAll()
 		{
 			return NewResult(await Mediator.Send(new GetListSkillsQuery()));
 
 		}
+
+
+		[SwaggerOperation(
+			Summary = "Create a new skill",
+			Description = "Adds a new skill to the system with the provided details.",
+			OperationId = "AddNewSkill")]
 
 		[HttpPost(Router.SkillRoute.Create)]
 		public async Task<IActionResult> AddNewSkill([FromBody] AddSkillCommand request)
@@ -34,6 +50,11 @@ namespace JobBoard.Api.Controllers
 
 		}
 
+		[SwaggerOperation(
+			Summary = "Update a skill",
+			Description = "Updates an existing skill with the provided information.",
+			OperationId = "UpdateSkill")]
+
 		[Authorize(Roles = "Admin")]
 		[HttpPut(Router.SkillRoute.Update)]
 		public async Task<IActionResult> UpdateSkill([FromBody] UpdateSkillCommand request)
@@ -41,6 +62,12 @@ namespace JobBoard.Api.Controllers
 			return NewResult(await Mediator.Send(request));
 
 		}
+
+
+		[SwaggerOperation(
+			Summary = "Delete a skill",
+			Description = "Deletes a skill identified by its unique ID.",
+			OperationId = "DeleteSkill")]
 
 		[Authorize(Roles = "Admin")]
 		[HttpDelete(Router.SkillRoute.DeleteById)]
