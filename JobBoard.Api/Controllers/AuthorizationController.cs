d6386c275df0523df156461b4970624ffa891bea
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-02 19:24:02 +0200

    Add Swagger Description For Authorization

--- JobBoard.Api/Controllers/AuthorizationController.cs
+++ JobBoard.Api/Controllers/AuthorizationController.cs
@@ -4,6 +4,7 @@ using JobBoard.Core.Feutures.Authorization.Queries.Models;
 using JobBoard.Data.Metadata;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace JobBoard.Api.Controllers
 {
@@ -11,6 +12,12 @@ namespace JobBoard.Api.Controllers
 	[Authorize(Roles = "Admin,SuperAdmin")]
 	public class AuthorizationController : AppControllerbase
 	{
+		[SwaggerOperation(Summary = "Get all roles",
+				  Description = "Returns a list of all roles defined in the system.",
+				  OperationId = "GetAllRoles")]
+
+
+
 		[HttpGet(Router.AuthorizationRoute.GetAllRoles)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -19,6 +26,10 @@ namespace JobBoard.Api.Controllers
 		{
 			return NewResult(await Mediator.Send(new GetListRolesQuery()));
 		}
+
+		[SwaggerOperation(Summary = "Get role by ID",
+				  Description = "Retrieves detailed information about a specific role by its ID.",
+				  OperationId = "GetRoleById")]
 
 		[HttpGet(Router.AuthorizationRoute.GetRoleById)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
@@ -30,6 +41,11 @@ namespace JobBoard.Api.Controllers
 		}
 
 		//Get  manage user roles
+
+		[SwaggerOperation(Summary = "Get user roles management data",
+				  Description = "Fetches role assignment details for a specific user.",
+				  OperationId = "GetManageUserRoles")]
+
 		[HttpGet(Router.AuthorizationRoute.GetManageUserRoles)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
@@ -41,6 +57,12 @@ namespace JobBoard.Api.Controllers
 		}
 
 		// Update user roles
+
+
+		[SwaggerOperation(Summary = "Update user roles",
+				  Description = "Updates the roles assigned to a specific user.",
+				  OperationId = "UpdateUserRoles")]
+
 		[HttpPut(Router.AuthorizationRoute.UpdateUserRoles)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
@@ -52,6 +74,12 @@ namespace JobBoard.Api.Controllers
 		}
 
 		// Add Role
+
+
+		[SwaggerOperation(Summary = "Add new role",
+				  Description = "Creates a new role in the system with the specified permissions.",
+				  OperationId = "AddRole")]
+
 		[HttpPost(Router.AuthorizationRoute.Create)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -60,6 +88,10 @@ namespace JobBoard.Api.Controllers
 		{
 			return NewResult(await Mediator.Send(request));
 		}
+
+		[SwaggerOperation(Summary = "Update role",
+				  Description = "Updates the details of an existing role.",
+				  OperationId = "UpdateRole")]
 
 		[HttpPut(Router.AuthorizationRoute.Update)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
@@ -73,6 +105,11 @@ namespace JobBoard.Api.Controllers
 			return NewResult(await Mediator.Send(request));
 		}
 
+
+		[SwaggerOperation(Summary = "Delete role",
+				  Description = "Deletes an existing role by ID.",
+				  OperationId = "DeleteRole")]
+
 		[HttpDelete(Router.AuthorizationRoute.DeleteById)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -85,6 +122,11 @@ namespace JobBoard.Api.Controllers
 		}
 
 
+
+		[SwaggerOperation(Summary = "Get user claims management data",
+				  Description = "Fetches claim assignment details for a specific user.",
+				  OperationId = "GetManageUserClaims")]
+
 		[HttpGet(Router.AuthorizationRoute.ManageUserClaims)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
@@ -94,6 +136,10 @@ namespace JobBoard.Api.Controllers
 		{
 			return NewResult(await Mediator.Send(new ManageUserClaimsQuery { UserId = Id }));
 		}
+
+		[SwaggerOperation(Summary = "Update user claims",
+				  Description = "Updates the claims assigned to a specific user.",
+				  OperationId = "UpdateUserClaims")]
 
 		[HttpPut(Router.AuthorizationRoute.UpdateUserClaims)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
