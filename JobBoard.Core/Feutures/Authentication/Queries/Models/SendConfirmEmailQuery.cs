386ae9bbc41e6ebd83fa1e122c13c5e6ce437d30
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-14 23:28:53 +0200

    Add Send Cinfirm Email

---
+++ JobBoard.Core/Feutures/Authentication/Queries/Models/SendConfirmEmailQuery.cs
@@ -0,0 +1,10 @@
+﻿using JobBoard.Core.Bases;
+using MediatR;
+
+namespace JobBoard.Core.Feutures.Authentication.Queries.Models
+{
+	public class SendConfirmEmailQuery(int userId) : IRequest<Response<string>>
+	{
+		public int UserId { get; set; } = userId;
+	}
+}
