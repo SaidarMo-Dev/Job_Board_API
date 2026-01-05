75d62b6a69d5cd4044f4ed8810a5e87b5dd3c395
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-05 13:55:17 +0100

    Fix Interfaces Problem

--- JobBoard.Service/Abstractions/IFileStrageService.cs
+++ JobBoard.Service/Abstractions/IFileStrageService.cs
@@ -4,20 +4,19 @@ namespace JobBoard.Service.Abstractions
 {
 	public interface IFileStrageService
 	{
-		public interface IFileStorageService
-		{
-			Task<string> UploadAsync<TId>(
-				Stream fileStream,
-				string fileName,
-				string contentType,
-				StorageResourceEnum resource,
-				TId resourceId)
-				where TId : notnull;
 
-			Task DeleteAsync(string filePath);
+		Task<string> UploadAsync<TId>(
+			Stream fileStream,
+			string fileName,
+			string contentType,
+			StorageResourceEnum resource,
+			TId resourceId)
+			where TId : notnull;
 
-			Task<string> CreateSignedReadUrlAsync(string filePath);
-		}
+		Task DeleteAsync(string filePath);
 
+		Task<string> CreateSignedReadUrlAsync(string filePath);
 	}
+
+
 }
