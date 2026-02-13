211aa58e526e998d51a8ef94fbb162db91956663
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-02-13 16:40:37 +0100

    feat(jobs): include company logo with jobs result

--- JobBoard.Api/obj/Debug/net8.0/JobBoard.Api.AssemblyInfo.cs
+++ JobBoard.Api/obj/Debug/net8.0/JobBoard.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobBoard.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d89f0570b0addbc4d1ece2c4d4dcfe9f211bfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03fc87eed640f3afabfb8f276b21dc3941c95df5")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobBoard.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobBoard.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
